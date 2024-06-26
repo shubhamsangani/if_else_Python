002ad0ab0493b9c0c61797f6aa91ea9b2fd0e98a
SHUBHAM_SANGANI <shubhamsangani5503@gmail.com>
2024-06-26 14:54:39 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,11 @@
+# conditional_if_else.py
+
+
+# initialize num_val with a numeric value
+num_val = 40
+
+# Check if num_val is more than 50 or not
+if (num_val > 50):
+  print("you scored above average")
+else:
+  print("You scored below average")
